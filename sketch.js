4499b418aa0f35fda98e35e067a894b870cdb48b
zarak5 <33879987+zarak5@users.noreply.github.com>
2019-04-30 11:21:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ Fast Style Transfer Simple Demo
 */
 
 let nets = {};
-let modelNames = ['la_muse', 'rain_princess', 'udnie', 'rose', 'scream', 'wave', 'mathura', 'fuchun', 'zhangdaqian'];
+let modelNames = ['la_muse', 'rain_princess', 'udnie', 'wreck', 'scream', 'wave', 'mathura', 'fuchun', 'zhangdaqian'];
 let inputImg, styleImg;
 let outputImgData;
 let outputImg;
